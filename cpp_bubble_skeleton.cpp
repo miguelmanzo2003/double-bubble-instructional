178d0dcbbcde7f5b90c25c06d84dda6e7e90c6dc
Miguel Manzo <miguelmanzo@MacBookAir.hsd1.ca.comcast.net>
2025-08-31 18:27:53 -0700

    Bubble Sort Completed with Design Requirements

--- cpp_bubble_skeleton.cpp
+++ cpp_bubble_skeleton.cpp
@@ -1,31 +1,76 @@
 //
 // template C++ code for doing bubblesort, W. Bethel, SFSU, 2/2023
 //
-// Your name here
-// Your course number, assignment number here
+// Miguel Manzo
+// Your course number, 
 // date here
-// 1-sentence description here: bubblesort skeleton code, used hand-crafted array of ints and a fixed problem size of n=10
+// 1-sentence description here: Use bubble sort to sort an array of 10 integers using a subroutine function
 //
 
 #include <iostream>
 #include <unistd.h>
 
-int
-main (int ac, char *av[])
-{
-   int a[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}; // replace with your own random 10 ints from www.calculator.net/random-number-generator.html
-   int n = sizeof(a)/sizeof(int);
 
-   std::cout << " a before sort " << std::endl;
+void print_array(int A[], int n){
+   for(int i = 0; i < n; i++){
+      std::cout << A[i] << " ";
+   }
+   std::cout << "\n";
+}
+
+int bubble_sort_inner(int A[], int N){
+
+   int changed = 0;
+   for(int j = 0; j < N; j++){
+
+         if(A[j] > A[j+1]){
+           int temp = A[j];
+           A[j] = A[j+1];
+           A[j+1]= temp;
+           changed = 1;
+           print_array(A, N);
+         }
+        
+   }
+   
+   return changed;
+}
+
+
+int main (int ac, char *av[])
+{
+   int a[] = {-2, -1, 0, 92, 52, 27, 86, 6, 52, 35}; // replace with your own random 10 ints from www.calculator.net/random-number-generator.html
+   int n = sizeof(a)/sizeof(int);
+   
+
+   std::cout << " a before sort "<< std::endl;
 
    // print out the contents of a before the sort
    // insert your code here
+   print_array(a, n);
+   
 
    // do the sort into ascending order. 
    // insert your code here
+   for(int i = 0; i < n-1; i++){
+
+
+
+      print_array(a, n);
+      int changed = bubble_sort_inner(a, n-i-1);
+      print_array(a, n);
+
+      if(changed == 0){
+         std::cout << "no changes were made\n\n";
+         break;
+      }
+
+   }
 
 
    // print out the contents of a after the sort
    // insert your code here
+   print_array(a, n);
+   return 0;
 }
 // EOF
